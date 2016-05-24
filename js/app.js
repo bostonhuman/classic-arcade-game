83ad337f51cf7fb0ab771e9c063783e4709a3e99
Edward Jornes <sethyathet@sethyathets-MacBook-Air.local>
2016-05-23 22:46:41 -0400

    app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-//Reset player to beginning position
-Object.prototype.reset = function() {
-  player.x = 200;
-  player.y = 400;
-};
+// Whole-script strict mode syntax
+"use strict";
 
 // Enemies the player must avoid
-var Enemy = function(x,y) {
-	// Variables applied to each of our instances go here,
+var Enemy = function(x, y) {
+    // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     // The image/sprite for enemies
     // a helper we've provided to easily load images
@@ -25,15 +22,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if(this.x <= 550){
+    if (this.x <= 550) {
         this.x += this.speed * dt;
-    }else{
+    } else {
         this.x = -2;
     }
 
     //If the player comes within 30px of an enemy's x and y coordinates, reset the game
-    if(player.x >= this.x - 30 && player.x <= this.x + 30){
-        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+    if (player.x >= this.x - 30 && player.x <= this.x + 30) {
+        if (player.y >= this.y - 30 && player.y <= this.y + 30) {
             this.reset();
         }
     }
@@ -43,62 +40,71 @@ Object.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 // Player class and initial x and y coordinates
-var Player = function(){
+var Player = function() {
     this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 400;
 };
 
+
+
 //Update player position
-Player.prototype.update = function(){
+Player.prototype.update = function() {
     //if left key is pressed and player is not on edge of map, pressed decrement x
-    if(this.ctlKey === 'left' && this.x > 0){ 
+    if (this.ctlKey === 'left' && this.x > 0) {
         this.x = this.x - 100;
-    //if right key is pressed and player is not on edge of map increment x 
-    }else if(this.ctlKey === 'right' && this.x != 400){
+        //if right key is pressed and player is not on edge of map increment x 
+    } else if (this.ctlKey === 'right' && this.x != 400) {
         this.x = this.x + 100;
-    //if up key is pressed increment y 
-    }else if(this.ctlKey === 'up'){
+        //if up key is pressed increment y 
+    } else if (this.ctlKey === 'up') {
         this.y = this.y - 85;
-    //if down key is pressed and player is not on edge of map decrement y 
-    }else if (this.ctlKey === 'down' && this.y != 400){
+        //if down key is pressed and player is not on edge of map decrement y 
+    } else if (this.ctlKey === 'down' && this.y != 400) {
         this.y = this.y + 85;
     }
     this.ctlKey = null;
-    
+
     //If on water, reset
-    if(this.y < 25){
-	    
-	    console.log("You won the game!!!");
+    if (this.y < 25) {
+
+        console.log("You won the game!!!");
         this.reset();
 
-        
+
     }
+};
+
+//Reset player to beginning position
+Object.prototype.reset = function() {
+    player.x = 200;
+    player.y = 400;
 };
 
 
 //Input handler for player
-Player.prototype.handleInput = function(e){
-    this.ctlKey = e;    
+Player.prototype.handleInput = function(e) {
+    this.ctlKey = e;
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Instantiate enemies and player objects
 var allEnemies = [];
-(function setEnemies(){
+(function setEnemies() {
     allEnemies.push(new Enemy(-2, 60));
     allEnemies.push(new Enemy(-2, 60));
-    allEnemies.push(new Enemy(-2,150));
-    allEnemies.push(new Enemy(-2,220));
+    allEnemies.push(new Enemy(-2, 150));
+    allEnemies.push(new Enemy(-2, 220));
 }());
 
 // Place the player object in a variable called player
-var player = new Player(); 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
